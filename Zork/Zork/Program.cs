dc2b422208f30e8d3b21484ebb6616f0061b639b
mshefer <42977579+mshefer@users.noreply.github.com>
2019-09-24 09:08:56 -0400

    4-Game Loop
    Assignment 4 Commit

--- Zork/Zork/Program.cs
+++ Zork/Zork/Program.cs
@@ -8,9 +8,20 @@ namespace Zork
 		{
 			Console.WriteLine("Welcome to Zork!");
 
-			string inputString = Console.ReadLine();
-			Commands command = ToCommand(inputString.Trim().ToUpper());
-			Console.WriteLine(command);
+			Commands command = Commands.UNKNOWN;
+			while (command != Commands.QUIT)
+			{
+				Console.Write("> ");
+				command = ToCommand(Console.ReadLine().Trim());
+
+				string outputString;
+				switch (command)
+				{
+				
+				}
+
+				Console.WriteLine(outputString);
+			}
 		}
 
 		private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
