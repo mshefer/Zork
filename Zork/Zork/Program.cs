28be538e723812f8d432759f28a32fa4346deff5
mshefer <42977579+mshefer@users.noreply.github.com>
2019-09-24 09:12:23 -0400

    6-The Game World
    Assignment 6 Commit

--- Zork/Zork/Program.cs
+++ Zork/Zork/Program.cs
@@ -17,7 +17,24 @@ namespace Zork
 				string outputString;
 				switch (command)
 				{
-				
+					case Commands.QUIT:
+						outputString = "Thank you for playing!";
+						break;
+
+					case Commands.LOOK:
+						outputString = "This is an open field west of a qhite house, with a boarded front door.\nA rubber may saying 'Welcome to Zork!' lies by the door.";
+						break;
+
+					case Commands.NORTH:
+					case Commands.SOUTH:
+					case Commands.EAST:
+					case Commands.WEST:
+						outputString = $"You moved {command}";
+						break;
+
+					default:
+						outputString = "Unknown command.";
+						break;
 				}
 
 				Console.WriteLine(outputString);
