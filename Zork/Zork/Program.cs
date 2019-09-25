9aee2f64ba8a3694f27f2962f44c45326b4fe43c
mshefer <42977579+mshefer@users.noreply.github.com>
2019-09-25 14:25:54 -0400

    8-Object Oriented Rooms
    Assignment 8 Commit

--- Zork/Zork/Program.cs
+++ Zork/Zork/Program.cs
@@ -6,7 +6,7 @@ namespace Zork
 	internal class Program
 	{
 
-		private static string CurrentRoom
+		private static Room CurrentRoom
 		{
 			get
 			{
@@ -16,6 +16,7 @@ namespace Zork
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Welcome to Zork!");
+			InitializeRoomDescriptions();
 
 			Commands command = Commands.UNKNOWN;
 			while (command != Commands.QUIT)
@@ -31,7 +32,7 @@ namespace Zork
 						break;
 
 					case Commands.LOOK:
-						Console.WriteLine("A rubber may saying 'Welcome to Zork!' lies by the door.");
+						Console.WriteLine(CurrentRoom.Description);
 						break;
 
 					case Commands.NORTH:
@@ -86,11 +87,26 @@ namespace Zork
 
 		private static bool IsDirection(Commands command) => Directions.Contains(command);
 
-		private static readonly string[,] Rooms = {
-			{ "Rocky Trail", "South of House", "Canyon View" },
-			{ "Forest", "West of House", "Behind House" },
-			{"Dense Woods", "North of House", "Clearing" }
+		private static readonly Room[,] Rooms = {
+			{ new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View") },
+			{ new Room("Forest"), new Room("West of House"), new Room("Behind House") },
+			{ new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") }
 		};
+
+		private static void InitializeRoomDescriptions()
+		{
+			Rooms[0,0].Description = "You are on a rock-strewn trail.";																						// Rocky Trail
+			Rooms[0,1].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred.";				// South of House
+			Rooms[0,2].Description = "You are at the top of the Great Canyon on its south wall.";															// Canyon View
+
+			Rooms[1,0].Description = "This is a forest, with trees in all directions around you.";															// Forest
+			Rooms[1,1].Description = "This is an open field west of a white house, with a boarded front door.";												// West of House
+			Rooms[1,2].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar.";			// Behind House
+
+			Rooms[2,0].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";					// Dense Woods
+			Rooms[2,1].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred.";				// North of House
+			Rooms[2,2].Description = "You are in a clearing, with a forest surrounding you on the west and south.";											// Clearing
+		}
 
 		private static readonly List<Commands> Directions = new List<Commands>
 		{
