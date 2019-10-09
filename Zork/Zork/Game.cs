5194ab93c21cde8af254f5b1b778034c4eb9330e
mshefer <42977579+mshefer@users.noreply.github.com>
2019-10-09 15:12:40 -0400

    End of Class Commit 12
    End of Class commit Assignment 12

---
+++ Zork/Zork/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zork
+{
+	public class Game
+	{
+		public World World { get;  }
+	}
+
+	public static Game Load(string filename)
+	{
+		Game game = JsonConvert.DeserializeObject<Game>(File.ReadAllText(filename));
+		return game;
+	}
+}
