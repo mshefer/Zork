7999bc839bfdae1adfd6e8f0b222bebd64ff4ba6
mshefer <42977579+mshefer@users.noreply.github.com>
2019-10-14 15:29:53 -0400

    12-end of class submit
    12-end of class submit/complete but with errors

--- Zork/Zork/Program.cs
+++ Zork/Zork/Program.cs
@@ -6,142 +6,22 @@ using Newtonsoft.Json;
 
 namespace Zork
 {
-	internal class Program
+	class Program
 	{
-
-		private static Room CurrentRoom
-		{
-			get
-			{
-				return Rooms[Location.Row, Location.Column];
-			}
-		}
-		private enum CommandLineArguments
-		{
-			RoomsFilename = 0
-		}
-
 		static void Main(string[] args)
 		{
+			const string defaultGameFilename = "Zork.json";
+			string GameFilename = (args.Length > 0 ? args[(int)CommandLineArguments.GameFilename] : defaultGameFilename);
+
+			Game game = Game.Load(gameFilename);
 			Console.WriteLine("Welcome to Zork!");
-
-			const string defaultRoomsFile = "Rooms.json";
-			string roomsFilename = (args.Length > 0 ? args[(int)CommandLineArguments.RoomsFilename] : defaultRoomsFile);
-
-			InitializeRooms(roomsFilename);
-
-			Room previousRoom = null;
-			Commands command = Commands.UNKNOWN;
-			while (command != Commands.QUIT)
-			{
-				Console.WriteLine(CurrentRoom);
-				if (previousRoom != CurrentRoom)
-				{
-					Console.WriteLine(CurrentRoom.Description);
-					previousRoom = CurrentRoom;
-				}
-				Console.Write("> ");
-				command = ToCommand(Console.ReadLine().Trim());
-
-				switch (command)
-				{
-					case Commands.QUIT:
-						Console.WriteLine("Thank you for playing!");
-						break;
-
-					case Commands.LOOK:
-						Console.WriteLine(CurrentRoom.Description);
-						break;
-
-					case Commands.NORTH:
-					case Commands.SOUTH:
-					case Commands.EAST:
-					case Commands.WEST:
-						if (Move(command) == false)
-						{
-							Console.WriteLine("The way is shut!");
-						}
-						break;
-
-					default:
-						Console.WriteLine("Unknown command.");
-						break;
-				}
-			}
+			game.Run();
+			Console.WriteLine("Thank you for playing!");
 		}
 
-		private static bool Move(Commands command)
+		private enum CommandLineArguments
 		{
-			Assert.IsTrue(IsDirection(command), "Invalid direction.");
-
-			bool isValidMove = true;
-			switch (command)
-			{
-				case Commands.NORTH when Location.Row > 0:
-					Location.Row--;
-					break;
-
-				case Commands.SOUTH when Location.Row < Rooms.GetLength(0) - 1:
-					Location.Row++;
-					break;
-
-				case Commands.EAST when Location.Column < Rooms.GetLength(1) - 1:
-					Location.Column++;
-					break;
-
-				case Commands.WEST when Location.Column > 0:
-					Location.Column--;
-					break;
-
-				default:
-					isValidMove = false;
-					break;
-			}
-
-			return isValidMove;
+			GameFilename = 0
 		}
-
-		private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
-
-		private static bool IsDirection(Commands command) => Directions.Contains(command);
-
-		private static Room[,] Rooms; /*= {
-			{ new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View") },
-			{ new Room("Forest"), new Room("West of House"), new Room("Behind House") },
-			{ new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") }
-		}; */
-
-		private static readonly Dictionary<string, Room> RoomMap;
-
-		/*
-		static Program()
-		{
-			RoomMap = new Dictionary<string, Room>();
-			foreach (Room room in Rooms)
-			{
-				RoomMap[room.Name] = room;
-			}
-		}
-		*/
-
-		private enum Fields
-		{
-			Name = 0,
-			Description
-		}
-		
-		private static void InitializeRooms(string roomsFilename) =>
-			Rooms = JsonConvert.DeserializeObject<Room[,]>(File.ReadAllText(roomsFilename));
-
-
-		private static readonly List<Commands> Directions = new List<Commands>
-		{
-			Commands.NORTH,
-			Commands.SOUTH,
-			Commands.EAST,
-			Commands.WEST
-		};
-
-		private static (int Row, int Column) Location = (1, 1);
 	}
 }
--- Zork/Zork/Room.cs
+++ Zork/Zork/Room.cs
@@ -1,22 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using Newtonsoft.Json;
 
 namespace Zork
 {
-	public class Room
+	public class Room : IEquatable<Room>
 	{
+		[JsonProperty(Order = 1)]
+		public string Name { get; private set; }
 
-		public string Name { get; }
+		[JsonProperty(Order = 2)]
+		public string Description { get; private set; }
 
-		public string Description { get; set; }
+		[JsonProperty(PropertyName = "Neighbors", Order = 3)]
+		private Dictionary<Directions, Room> NeighborNames { get; private set; }
 
-		public override string ToString() => Name;
+		[JsonIgnore]
+		public IReadOnlyDictionary<Directions, Room> Neighbors { get; private set; }
 
-		public Room(string name, string description = "")
+		public static bool operator == (Room lhs, Room rhs)
 		{
-			Name = name;
-			Description = description;
+			if (ReferenceEquals(lhs,rhs))
+			{
+				return true;
+			}
+			if (lhs is null|| rhs is null)
+			{
+				return false;
+			}
+
+			return lhs.Name == rhs.Name;
 		}
+
+		public static bool operator !=(Room lhs, Room rhs) => !(lhs == rhs);
+		public override bool Equals(object obj) => obj is Room room ? this == room : false;
+		public bool Equals(Room other) => this == other;
+		public override string ToString() => Name;
+		public override int GetHashCode() => Name.GetHashCode();
+		public void UpdateNeighbors(World world) => Neighbors = (from entry in NeighborNames
+																 let room = world.RoomsByName.GetValueOrDefault(entry.Value)
+																 where room != null
+																 select (Direction: entry.Key, Room: room))
+																 .ToDictionary(pair => pair.Direction, KeyValuePair => KeyValuePair.Room);
+
 	}
 }
